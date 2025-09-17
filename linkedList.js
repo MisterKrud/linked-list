90589ccd3e03c5a550dc09b5e214f7951e70fe73
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-09-17 20:57:34 +1000

    Add listLength returns

--- linkedList.js
+++ linkedList.js
@@ -2,3 +2,36 @@
 
 console.log('test')
 
+const linkedList = () =>{
+
+    let listLength  = 0;
+  
+    
+
+    const append=(value) => {
+        listLength ++
+       return  newNode(value, null);
+    }
+    const prepend=(value) =>{
+        listLength++
+    return newNode(null, value);
+    }
+    const size = () => listLength+1
+    const head = () => newNode(null, newNode.getNext());
+    const tail = () => newNode(null, null);
+    const atIndex = () => newNode.getValue()//unfinished
+    const pop = () => {
+        const tailNode = tail();
+        listLength--
+        //get node before tail
+        //replace next value of previous node to 'null'
+    }
+}
+
+
+const newNode = (value=null, next=null) => {
+    const getValue = () => value;
+    const getNext = () => next;
+
+    return {getValue, getNext}
+}
