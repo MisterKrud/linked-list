b443a907fb01e68c4d1005efe58988e305275e6e
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-09-22 21:42:14 +1000

    Add prepend, head, tail, at and pop

--- linkedList.js
+++ linkedList.js
@@ -1,108 +1,102 @@
 #!/usr/bin/node
 
+const linkedList = () => {
+  let n = 0;
 
+  let preNode = node();
 
-const linkedList = () =>{
-
-    let n  = 0;
-    const getListLength = () => n
-  
-  const headNode = node(null, null);
-  
-
-
- const append = (value) => {
-    
-const newNode =   node(value) 
-    newNode.index = n
-
-    let current = headNode
-
-   
-    while (current.next != null){
-        
-    
-       
-        current = current.next
-        
+  const append = (value) => {
+    const newNode = node(value);
+    newNode.index = n;
+    let current = preNode;
+    while (current.next) {
+      current = current.next;
     }
 
-   if(current.next === null){
-    current.next = newNode
-}
-// console.log('---------')
-//         console.log(current)
-//         console.log(current.next)
-//         console.log('__________')
-//         console.log('')
-   
+    if (current.next === null) {
+      current.next = newNode;
+    }
+    n++;
+    return newNode;
+  };
 
-    //while ever the current.next is not null
-    //current node = current.next
-        // console.log(newNode)
+  const prepend = (value) => {
+    const newNode = node(value);
+    const rightNode = preNode.next;
+    newNode.next = rightNode;
+    preNode.next = newNode;
+    newNode.index = 0;
+    let current = newNode;
+    while (current.next != null) {
+      current = current.next;
+      current.index++;
+    }
+  };
 
-         n++
-   return newNode
-}
+  const size = () => n;
 
-const getNodeByIndex = (n) => {
-   console.log(node(n))
-}
-//     const prepend=(value) =>{
-//         listLength++
-//     return node(null, value);
-//     }
-
-
-    const size = () => n
-
-   
-// // let head = () => node(null, next)
-
-//     const tail =  node(null, null);
-
-//     const at = (index) => node.getValue()//unfinished - returns node at the index
-
-//     const pop = () => {
-//         const tailNode = tail();
-//         listLength--
-//         //get node before tail
-//         //replace next value of previous node to 'null'
-//     }
-
-//     const contains = (value) =>{
-//         //returns true if value is in linkedList. Otherwise, false
-//     }
-
-//     const find = (value) => {
-//         //if node contains value, return node
-//         //else null
-//     }
-
-    const toString = () => {
-        let current = headNode.next;
-   
-        while(current !=null) {
-           
-     console.log(`${current.value} ->`)
-     current = current.next
-        }
-      
-    
-}
+  const head = () => {
+    const headNode = preNode.next
+    return headNode.value;
+  }
   
 
-    return {append, toString, size}
-}
-
-
-const node = (value=null, next=null) => {
-    const getValue = () => value;
-    const getNext = () => next;
+  const tail = () => {
+    let current = preNode;
+    while(current.next) {
+        current = current.next
+        if (current.next === null){
+            const tailNode = current
+            return tailNode.value
+        }
     
+    }
+  }
 
-    return {value, next, getValue, getNext}
-}
+
+
+  const at = (idx) => {
+    let current = preNode;
+    while(current.next){
+        current = current.next;
+        if(current.index === idx){
+            const atNode = current;
+            return atNode.value;
+        }
+    }
+  }
+
+ 
+  const pop = () => {
+    
+    let current = preNode;
+    while(current.next){
+        current = current.next
+        let currentNext = current.next
+        if(currentNext.next === null){
+            current.next = null
+        }
+    }
+  }
+
+  const toString = () => {
+    let current = preNode.next;
+
+    while (current != null) {
+      console.log(`${current.value} -> ${current.index}`);
+      current = current.next;
+    }
+  };
+
+  return { prepend, append, toString, size, head, tail, at, pop };
+};
+
+const node = (value = null, next = null) => {
+  const getValue = () => value;
+  const getNext = () => next;
+
+  return { value, next, getValue, getNext };
+};
 
 const list = linkedList();
 list.append("dog");
@@ -111,16 +105,22 @@ list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
+list.prepend("car");
 
 // console.log(list.append.newNode)
 
-console.log('toString:')
-console.log(list.toString())
+console.log("toString:");
+console.log(list.toString());
 
-console.log(list.size())
+console.log(list.size());
+console.log(list.head())
+console.log(list.tail())
+console.log(list.at(5))
+list.pop()
+
+console.log(list.toString());
 
 // const firstNode = node("One");
-
 
 // const secondNode = node("Two");
 // console.log(firstNode.value)
@@ -128,7 +128,5 @@ console.log(list.size())
 // console.log(firstNode)
 // firstNode.next = "Two"
 // console.log({secondNode})
-
-
 
 //head.next = null
